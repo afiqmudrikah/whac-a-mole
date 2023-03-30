43cdfb2be2e3fbdd9a305cce42d10893b0dec050
afiqmudrikah <afiqmudrikah@hotmail.com>
2023-03-31 06:57:28 +0800

    Setting initialize function
    1) Added h2 element that will display points and a start button.
    2) Added initialize function that can be used as a callback function to initialize the game.
    3) Testing initGame() to check for variables that can be initialized.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const grid = document.querySelectorAll('.grid');
-
+const displayPoints = document.querySelector('#points');
+let startBtn = document.querySelector('#startBtn');
 let points = 0;
+
+// initialize game
+function initGame() {
+  for (const target of grid) {
+    target.removeAttribute('style', 'background-color: red');
+  }
+  displayPoints.textContent = `Points: ${(points = 0)}`;
+  setInterval(generateTarget, 5000);
+}
+
+startBtn.addEventListener('click', initGame);
 
 function generateTarget() {
   for (const target of grid) {
@@ -10,26 +22,22 @@ function generateTarget() {
   const randomNum = Math.floor(Math.random() * 8); // generates pseudo-random integer
   const newTarget = gridTarget[randomNum];
   newTarget.setAttribute('style', 'background-color: red');
-  // console.log(randomNum);
 }
 
 function targetHit() {
   for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener('click', function () {
       if (grid[i].hasAttribute('style', 'background-color: red')) {
-        alert('Hit!');
+        // alert('Hit!');
         points++;
         generateTarget();
-        console.log(points);
+        displayPoints.textContent = `Points: ${points}`;
       } else {
         points--; // penalty if user misclicks
         generateTarget();
-        console.log(points);
+        displayPoints.textContent = `Points: ${points}`;
       }
     });
   }
 }
-
 targetHit();
-
-setInterval(generateTarget, 2500);
