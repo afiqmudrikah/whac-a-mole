1938581f7c8720de3f6c202bad3249cc80f6e214
afiqmudrikah <afiqmudrikah@hotmail.com>
2023-03-31 04:00:04 +0800

    Changed grid layout & additional functions
    1) Changed CSS display to grid and display a 3x3 'grid' layout.
    2) Introduced a for loop that removes a specified attribute at the start of  generateTarget() & added a setInterval method to call the function. The mentioned for loop resets the grid every time generateTarget() is called.
    4) Added a points variable that will register a point whenever the correct target is 'clicked' and calls the generateTarget() to randomize the next grid.

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const grid = document.querySelectorAll('.grid');
 
+let points = 0;
+
 function generateTarget() {
+  for (const target of grid) {
+    target.removeAttribute('style', 'background-color: red');
+  }
   const gridTarget = document.querySelectorAll('.grid');
-  const randomNum = Math.floor(Math.random() * 2); // generates pseudo-random integer
+  const randomNum = Math.floor(Math.random() * 8); // generates pseudo-random integer
   const newTarget = gridTarget[randomNum];
   newTarget.setAttribute('style', 'background-color: red');
-  console.log(randomNum);
+  // console.log(randomNum);
 }
-
-generateTarget();
 
 function targetHit() {
   for (let i = 0; i < grid.length; i++) {
     grid[i].addEventListener('click', function () {
       if (grid[i].hasAttribute('style', 'background-color: red')) {
         alert('Hit!');
+        points++;
+        generateTarget();
+        console.log(points);
       }
     });
   }
 }
 
 targetHit();
+
+setInterval(generateTarget, 1000);
