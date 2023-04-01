10d487dd48201d7724d07321abee4f84b944f8a8
afiqmudrikah <afiqmudrikah@hotmail.com>
2023-04-01 18:45:58 +0800

    Creating game timer
    1) Added a h2 element to display the current game time.
    2) Added the countdown() that will run down the game time from initial value to 0 and will stop generating new target.

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const displayPoints = document.querySelector('#points');
 let startBtn = document.querySelector('#startBtn');
 let points = 0;
 
+let timer;
+let movingTarget;
+let currentTime = 5;
+const timeLimit = document.querySelector('#time-limit');
+
 // initialize game
 function initGame() {
   for (const target of grid) {
     target.removeAttribute('style', 'background-color: red');
   }
   displayPoints.textContent = `Points: ${(points = 0)}`;
-  setInterval(generateTarget, 5000);
+  movingTarget = setInterval(generateTarget, 2500);
+  timer = setInterval(countdown, 1000);
 }
 
 startBtn.addEventListener('click', initGame);
@@ -34,10 +40,21 @@ function targetHit() {
         displayPoints.textContent = `Points: ${points}`;
       } else {
         points--; // penalty if user misclicks
-        generateTarget();
         displayPoints.textContent = `Points: ${points}`;
       }
     });
   }
 }
 targetHit();
+
+function countdown() {
+  currentTime--;
+  timeLimit.textContent = currentTime;
+
+  if (currentTime === 0) {
+    console.log('Suppose to stop');
+    clearInterval(timer);
+    clearInterval(movingTarget);
+    alert(`Game is over, your points is ${points}`);
+  }
+}
